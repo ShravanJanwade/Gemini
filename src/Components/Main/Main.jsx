2f533cdbeb3d8cab25eb5580fe894cfd30b0c9f9
Shravankumar Janwade <shravanjanwade2252@gmail.com>
2024-05-23 21:45:48 +0530

    Sidebar remaining

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Main.css";
-
+import { Context } from "../../context/ContextApi";
 const Main = () => {
+  const {
+    onSent,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(Context);
+
   return (
     <div className="main">
       <div className="nav">
@@ -9,40 +19,72 @@ const Main = () => {
         <img src={assets.user_icon} />
       </div>
       <div className="main-container">
-        <div className="greet">
-          <p>
-            <span>Hello, Dev.</span>
-          </p>
-          <p>How can I help you today?</p>
-        </div>
-        <div className="cards">
-          <div className="card">
-            <p>Suggest beautiful places to see on an upcoming travel.</p>
-            <img src={assets.compass_icon} />
+        {!showResult ? (
+          <>
+            {" "}
+            <div className="greet">
+              <p>
+                <span>Hello, Dev.</span>
+              </p>
+              <p>How can I help you today?</p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>Suggest beautiful places to see on an upcoming travel.</p>
+                <img src={assets.compass_icon} />
+              </div>
+              <div className="card">
+                <p>Suggest beautiful places to see on an upcoming travel.</p>
+                <img src={assets.bulb_icon} />
+              </div>
+              <div className="card">
+                <p>Suggest beautiful places to see on an upcoming travel.</p>
+                <img src={assets.message_icon} />
+              </div>
+              <div className="card">
+                <p>Suggest beautiful places to see on an upcoming travel.</p>
+                <img src={assets.code_icon} />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <img src={assets.user_icon} />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }} />
+              )}
+            </div>
           </div>
-          <div className="card">
-            <p>Suggest beautiful places to see on an upcoming travel.</p>
-            <img src={assets.bulb_icon} />
-          </div>
-          <div className="card">
-            <p>Suggest beautiful places to see on an upcoming travel.</p>
-            <img src={assets.message_icon} />
-          </div>
-          <div className="card">
-            <p>Suggest beautiful places to see on an upcoming travel.</p>
-            <img src={assets.code_icon} />
-          </div>
-        </div>
+        )}
+
         <div className="main-bottom">
           <div className="search-box">
-            <input type="text" placeholder="Enter a prompt here" />
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              type="text"
+              placeholder="Enter a prompt here"
+            />
             <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
-              <img src={assets.send_icon} alt="" />
+              <img onClick={() => onSent()} src={assets.send_icon} alt="" />
             </div>
           </div>
-          <p className="bottom-info">Gemini may display inaccurate info,take it into consideration.</p>
+          <p className="bottom-info">
+            Gemini may display inaccurate info,take it into consideration.
+          </p>
         </div>
       </div>
     </div>
